54e7d9ce484a8c2506d6c899ea5f36e898cb0821
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-10-18 14:08:41 +0530

    initial commit

---
+++ Coding/sprint 2/Evaluation/app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Join a group to meet people, make friends, find support, grow a business, and explore your interests. Thousands of events are happening every day, both online and in person!</h1>
+    </div>
+  );
+}
+
+export default App;
