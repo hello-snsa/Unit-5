7ffda68f8a10e217c1a46e4908ecf0ea5b01e5c1
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-08 16:22:21 +0530

    commit -10

--- Coding/sprint 4/evaluation/src/middlewares/verifyToken.js
+++ Coding/sprint 4/evaluation/src/middlewares/verifyToken.js
@@ -1,1 +1,30 @@
-// verifyAuth
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SEC_KEY, (err, user) => {
+            if (err) {
+                return res.status(401).json("Token invalid" + err)
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(401).json("you are not authorized to perform this action.")
+    }
+};
+
+const verifyAuth = (req, res, next) => {
+    verifyToken(req, res, async () => {
+        if (req.user.isAdmin || req.user.isInstructor) {
+            next();
+        } else {
+            return res.status(401).json("You are not authorized");
+        }
+    });
+};
+
+module.exports = { verifyAuth };
