156102acfd9ee5bd32afce88dfc68d25fb54437c
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-08 14:54:21 +0530

    commit -4

--- Coding/sprint 4/evaluation/src/controllers/lecture.controller.js
+++ Coding/sprint 4/evaluation/src/controllers/lecture.controller.js
@@ -9,3 +9,19 @@ router.post("/", verifyAuth, async (req, res) => {
     const lecture = await Lecture.create(req.body);
     return res.status(201).json({ lecture });
 })
+
+router.get("/", async (req, res) => {
+    const lectures = await Lecture.find().populate("instructor_id").lean().exec();
+    return res.status(201).json({ lectures });
+});
+
+router.get("/:id", async (req, res) => {
+    const lectures = await Lecture.findById(req.params.id).populate("instructor_id");
+    .lean()
+        .exec();
+
+
+    return res.status(201).json({ lectures });
+});
+
+
