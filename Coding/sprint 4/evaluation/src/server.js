488361151c690d420c96cde57cccc900c939145e
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-08 15:58:08 +0530

    commit -9

--- Coding/sprint 4/evaluation/src/server.js
+++ Coding/sprint 4/evaluation/src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const connect = require('./configs/db');
 
+const userController = require('./controller/user.controller');
+
+const lectureController = require('./controller/lecture.controller');
 
 const app = express();
 app.use(express.json());
 
+app.use('/users', userController);
+app.use('/students', studentController);
+app.use('/lectures', lectureController);
 
 app.listen(2244, async () => {
     await connect();
